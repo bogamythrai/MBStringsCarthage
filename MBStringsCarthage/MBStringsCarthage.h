2569369bdf8f6b5aba1a51305889f377d3dda71c
Boga Mythrai <Mythrai.Boga@walmart.com>
2022-07-27 16:29:25 +0530

    Initial Commit

---
+++ MBStringsCarthage/MBStringsCarthage.h
@@ -0,0 +1,18 @@
+//
+//  MBStringsCarthage.h
+//  MBStringsCarthage
+//
+//  Created by Mythrai Boga on 27/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MBStringsCarthage.
+FOUNDATION_EXPORT double MBStringsCarthageVersionNumber;
+
+//! Project version string for MBStringsCarthage.
+FOUNDATION_EXPORT const unsigned char MBStringsCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBStringsCarthage/PublicHeader.h>
+
+
